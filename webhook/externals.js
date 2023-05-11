98efbf190ed5b174b6c6f771f22c11f6b9f5fb75
Shubham Baranwal <41940290+Sbwal@users.noreply.github.com>
2023-05-11 09:43:54 +0000

    externals

--- webhook/externals.js
+++ webhook/externals.js
@@ -1,20 +1,25 @@
 const debug = require('debug')('webhook:externals');
 const axios = require("axios").default;
+const config = require('./config/default');
 
-const options = {
-  method: 'POST',
-  url: 'https://bots.kore.ai/chatbot/hooks/st-dd9323e7-7886-511b-b363-a0c764f5bcb5',
-  headers: {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWJmODI1ZTEwLTgzZjAtNWY5Mi1hZWU3LTljOWU0NjExMTU1YiJ9.XRWSeoIjke_lVyDffxOxyEntIGfISapY6KTpuIO303M'
-  },
-  data: {message: {text: 'EMP001'}, from: {id: '8507967113'}}
-};
+const koreQueryResolver = (body) => {
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
+    const options = {
+        method: 'POST',
+        url: `https://${config.kore.bot.url}/chatbot/hooks/${config.kore.bot.id}`,
+        headers: {
+            Accept: '*/*',
+            'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${config.kore.bot.jwt}`
+        },
+        data: body
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+}
